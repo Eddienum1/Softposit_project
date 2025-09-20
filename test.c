bc0bb2cea77313a5788fdf2ca0f466ade44d6f52
Eddie <eddie20040405@gmail.com>
2025-09-20 15:49:23 +0800

    add infinity, -infinity, nan comparison

--- test.c
+++ test.c
@@ -1,11 +1,19 @@
 #include "softposit.h"
 #include <math.h>
+#include <inttypes.h>
+#include <string.h>
+
+uint64_t double_to_u64(double d){
+    uint64_t u;
+    memcpy(&u, &d, sizeof u);
+    return u;
+}
 
 int main() {
 /*
     int N = 20;
-    double approx_log = (1.0 - pow(10.0, -N)) / 9.0; // ~1/9
-    double exact = exp(approx_log);
+    double ans = (1.0 - pow(10.0, -N)) / 9.0; // ~1/9
+    double exact = exp(ans);
 
     float prod_f = 1.0f;
     for (int n = 1; n <= N; n++) {
@@ -38,6 +46,13 @@ int main() {
     double d1 = 1.0;
     double d2 = nextafter(d1, 2.0);//The next nubmer that can display from 1.0 to 2.0
     double double_diff = d2 - d1;
+    uint64_t bits;
+
+    bits = double_to_u64(d1);
+    printf("d1 in Hex is 0x%016" PRIx64 "\n", bits);
+    bits = double_to_u64(d2);
+    printf("d2 in Hex is 0x%016" PRIx64 "\n", bits);
+    printf("\n");
 
     //Posit64 ULP
     posit64_t p1, p2;
@@ -48,10 +63,10 @@ int main() {
     u.ui += 1;
     p2 = u.p;
 
-    printf("pd1 in Hex is ");
+    printf("p1 in Hex is ");
     printHex(p1.v);
 
-    printf("pd2 in Hex is ");
+    printf("p2 in Hex is ");
     printHex(p2.v);
  
     double pd1 = convertP64ToDouble(p1);
@@ -62,15 +77,44 @@ int main() {
     //The output shows that double can represent the small difference (ULP) near 1.0
     //However, the difference between two adjacent posit64 numbers near 1.0 is smaller than double's precision
     //So when converting to double, this tiny difference cannot be displayed
-    printf("Double @1.0: d1 = %.17f\n", d1);
-    printf("Double candidate: d2 = %.17f\n", d2);
+    printf("Double at 1.0: d1 = %.20f\n", d1);
+    printf("Double next number: d2 = %.20f\n", d2);
     printf("Double diff = %.20e\n", double_diff);//should be a very small number
 
     printf("\n");
 
-    printf("Posit64 @1.0: p1 = %.17f\n", pd1);
-    printf("Posit64 candidate: p2 = %.17f\n", pd2);
+    printf("Posit64 at 1.0: p1 = %.20f\n", pd1);
+    printf("Posit64 next number: p2 = %.20f\n", pd2);
     printf("Posit64 diff = %.20e\n", pd2 - pd1);//should be 0
+
+    printf("\n");
+
+    //It shows that we do not need three different bit pattern to represent infinity, -infinity, and Nan in posit system
+    posit64_t pA;
+    double dA;
+    dA = INFINITY;
+    bits = double_to_u64(dA);
+    pA = convertDoubleToP64(dA);
+    printf("double infinity is 0x%016" PRIx64 "\n", bits);
+    printf("Posit64 infinity is ");
+    printHex(pA.v);
+    printf("\n");
+
+    dA = -INFINITY;
+    bits = double_to_u64(dA);
+    pA = convertDoubleToP64(dA);
+    printf("double negative infinity is 0x%016" PRIx64 "\n", bits);
+    printf("Posit64 negative infinity is ");
+    printHex(pA.v);
+    printf("\n");
+
+    dA = NAN;
+    bits = double_to_u64(dA);
+    pA = convertDoubleToP64(dA);
+    printf("double NAN is 0x%016" PRIx64 "\n", bits);
+    printf("Posit64 NAN is ");
+    printHex(pA.v);
+    printf("\n");
 
     return 0;
 }
